708ad9860c30a69c53cccdcf70b5ad61cc9229f7
joangnz <joangnz@gmail.com>
2025-03-18 20:03:54 +0100

    Adequation of imports and routes for all exercises.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { BrowserRouter as Router, Route, Routes } from "react-routerdom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-import Ejercicio1 from "./components/Ejercicio1";
-import Ejercicio2 from "./components/Ejercicio2";
-import Ejercicio3 from "./components/Ejercicio3";
-import Ejercicio4 from "./components/Ejercicio4";
-import Ejercicio5 from "./components/Ejercicio5";
+import Ex1 from "./components/Ex1";
+import Ex2 from "./components/Ex2";
+import Ex3 from "./components/Ex3";
+import Ex4 from "./components/Ex4";
+import Ex5 from "./components/Ex5";
 import Footer from "./components/Footer";
 function App() {
   return (
@@ -14,11 +14,11 @@ function App() {
       <div className="container mx-auto p-4">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/ejercicio1" element={<Ejercicio1 />} />
-          <Route path="/ejercicio2" element={<Ejercicio2 />} />
-          <Route path="/ejercicio3" element={<Ejercicio3 />} />
-          <Route path="/ejercicio4" element={<Ejercicio4 />} />
-          <Route path="/ejercicio5" element={<Ejercicio5 />} />
+          <Route path="/ex1" element={<Ex1 />} />
+          <Route path="/ex2" element={<Ex2 />} />
+          <Route path="/ex3" element={<Ex3 />} />
+          <Route path="/ex4" element={<Ex4 />} />
+          <Route path="/ex5" element={<Ex5 />} />
         </Routes>
       </div>
       <Footer />
