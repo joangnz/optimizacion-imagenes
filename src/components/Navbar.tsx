c1bf07b7e61d1f78fbe884d01fdd88a3d9daf5a4
joangnz <joangnz@gmail.com>
2025-03-21 17:09:13 +0100

    Creation of Navbar CSS and fixed much of Navbar.tsx code.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom";
+import "./Navbar.css";
+
 const Navbar = () => {
     return (
         <nav className="bg-blue-500 p-4 text-white flex justify-between">
             <h1 className="text-xl font-bold">Optimización de Imágenes</h1>
             <div>
                 <Link className="mr-4" to="/">Inicio</Link>
-                <Link className="mr-4" to="/ejercicio1">Ejercicio 1</Link>
-                <Link className="mr-4" to="/ejercicio2">Ejercicio 2</Link>
-                <Link className="mr-4" to="/ejercicio3">Ejercicio 3</Link>
-                <Link className="mr-4" to="/ejercicio4">Ejercicio 4</Link>
-                <Link className="mr-4" to="/ejercicio5">Ejercicio 5</Link>
+                <Link className="mr-4" to="/ex1">Ejercicio 1</Link>
+                <Link className="mr-4" to="/ex2">Ejercicio 2</Link>
+                <Link className="mr-4" to="/ex3">Ejercicio 3</Link>
+                <Link className="mr-4" to="/ex4">Ejercicio 4</Link>
             </div>
         </nav>
     );
