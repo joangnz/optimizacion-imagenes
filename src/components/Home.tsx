12faf0a9eff2407f38e748a633c15dde6c7bd59d
joangnz <joangnz@gmail.com>
2025-03-21 17:02:16 +0100

    Initial coding of Home.tsx

---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+    return (
+        <div className="text-center">
+        <h1 className="text-4xl font-bold">Home</h1>
+        <p className="text-lg">Welcome to the Home page.</p>
+        </div>
+    );
+}
+
+export default Home;
